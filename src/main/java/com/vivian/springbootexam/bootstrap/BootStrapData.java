fcaee9780eeab19cdf97b5aec1a98597ebf5567f
Enez Mutluoğlu <44931036+EnezMutluoglu@users.noreply.github.com>
2021-10-02 17:54:38 +0300

    bootstrap

---
+++ src/main/java/com/vivian/springbootexam/bootstrap/BootStrapData.java
@@ -0,0 +1,43 @@
+package com.vivian.springbootexam.bootstrap;
+
+import com.vivian.springbootexam.domain.Author;
+import com.vivian.springbootexam.domain.Book;
+import com.vivian.springbootexam.repositories.AuthorRepository;
+import com.vivian.springbootexam.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception{
+
+        Author eric = new Author("Eric","Evans");
+        Book ddd =new Book("Domain Driven Desing","123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod","Johnson");
+        Book noEJB = new Book("J2EE Development without EJB","3266589654");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+    }
+}
